f8afd4bdd43e296389ad47a0734f3936d57d433a
Boris Lebon <bo.lebon@gmail.com>
2022-12-13 12:13:16 +0100

    fix: expose core/script

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
 export { INITIALIZATION_CONTEXT, CircularReferenceError, UnknownModuleError } from './kernel';
 export * from './module';
 export * from './primitive-container';
+export * from './script';
 export * from './utils/cli';
